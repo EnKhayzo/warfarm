5dcb6a1c9d6167cc12453a23683d79592afb492d
enkhayzo <enkhayzo@gmail.com>
2024-08-28 22:05:35 +0200

    dataset rework

--- worker.js
+++
@@ -1,6 +0,0 @@
-self.onmessage = function(event) {
-    const { data } = event;
-    // Perform some heavy computation or task here
-    const result = data * 2; // Example task
-    self.postMessage(result);
-};
