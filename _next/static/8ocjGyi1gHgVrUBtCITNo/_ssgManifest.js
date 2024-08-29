59396b9cd629790ee4450a6821e9f4b6d9b307f1
enkhayzo <enkhayzo@gmail.com>
2024-08-29 02:48:29 +0200

    dataset refactor fix

--- _next/static/8ocjGyi1gHgVrUBtCITNo/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fprime\u002F[category]","\u002Fprime\u002F[category]\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/LIHYh3ccZHTRJTN1YZndE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fprime\u002F[category]","\u002Fprime\u002F[category]\u002F[routeId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
